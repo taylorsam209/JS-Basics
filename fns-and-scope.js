a08388c49faa523f0a3b2dccd057e9db4ea44109
Taylor P. Sam <taylorsam209@yahoo.com>
2017-09-04 18:43:46 -0600

    Solved Challenge 1

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,6 +5,13 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+  function isTyler(name){
+    if(name === 'Tyler'){
+      return true;
+    }
+    else
+      return false;
+  }
 
 //////////////////PROBLEM 2////////////////////
 
@@ -13,7 +20,7 @@ var name = 'Tyler';
 
 
   //Code Here
-
+function getName
 
 //////////////////PROBLEM 3////////////////////
 
